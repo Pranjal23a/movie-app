a382ba727ed01ca2889921a71ef4ba4b1f610254
Pranjal Sharma <sharmapranjal23a@gmail.com>
2023-09-19 17:57:10 +0530

    Successfully added style using styled component and cart counter is dynamic now

--- src/MovieList.js
+++ src/MovieList.js
@@ -1,99 +1,22 @@
-import { Component } from "react";
-import MoviCard from "./Moviecard";
+import MovieCard from "./MovieCard";
 
-class MovieList extends Component {
-    constructor() {
-        super();
+function MovieList(props) {
 
-        this.state = {
-            movies: [
-                {
-                    id: 1,
-                    title: 'The Avengers',
-                    plot: 'Supernatural powers shown in the movie.',
-                    poster: "https://shorturl.at/eFGOR",
-                    price: 199,
-                    rating: 8.9,
-                    stars: 0,
-                    fav: false,
-                    cart: false,
-                },
-                {
-                    id: 2,
-                    title: 'Gadar 2',
-                    plot: 'India-Pakistan Border Story',
-                    poster: "https://shorturl.at/dilTY",
-                    price: 250,
-                    rating: 5.9,
-                    stars: 0,
-                    fav: false,
-                    cart: false,
-                },
-                {
-                    id: 3,
-                    title: 'Rocky aur Rani ki prem kahani',
-                    plot: 'Some moral stories shown in movie.',
-                    poster: "https://shorturl.at/cipqP",
-                    price: 550,
-                    rating: 9.2,
-                    stars: 0,
-                    fav: false,
-                    cart: false,
-                }
+  const { movies, onIncStars, onDecStars, onClickFav, onClickAddtocart } = props;
 
-            ]
+  return (
+    <div className="main">
+      {movies.map((movie, index) => (
+        <MovieCard movies={movie}
+          key={index}
+          onIncStars={onIncStars}
+          onDecStars={onDecStars}
+          onClickFav={onClickFav}
+          onClickAddtocart={onClickAddtocart} />
 
-        }
-    }
-    addStars = (movie) => {
-        const { movies } = this.state;
-        const mid = movies.indexOf(movie);
-        if (movies[mid].stars >= 5) {
-            return;
-        }
-        movies[mid].stars += 0.5;
-        this.setState({
-            movies: movies
-        });
-    }
-
-    decStars = (movie) => {
-        const { movies } = this.state;
-        const mid = movies.indexOf(movie);
-        if (movies[mid].stars <= 0) {
-            return;
-        }
-        movies[mid].stars -= 0.5;
-        this.setState({
-            movies: movies
-        });
-    }
-    handleFav = (movie) => {
-        const { movies } = this.state;
-        const mid = movies.indexOf(movie);
-        movies[mid].fav = !movies[mid].fav;
-        this.setState({
-            movies
-        });
-    }
-    handleCart = (movie) => {
-        const { movies } = this.state;
-        const mid = movies.indexOf(movie);
-        movies[mid].cart = !movies[mid].cart;
-        this.setState({
-            movies
-        });
-    }
-
-    render() {
-        const { movies } = this.state;
-        return (
-            <>
-                {movies.map((movie) => <MoviCard movies={movie} addStars={this.addStars}
-                    decStars={this.decStars} handleFav={this.handleFav} handleCart={this.handleCart} key={movie.id} />)}
-
-            </>
-        )
-    }
+      ))}
+    </div>
+  )
 }
+
 export default MovieList;
--- src/Moviecard.js
+++ src/Moviecard.js
@@ -1,41 +1,73 @@
-import { Component } from "react";
+function MovieCard(props) {
 
-class MovieCard extends Component {
+    const { movies, onIncStars, onClickFav, onClickAddtocart, onDecStars } = props
+    const { title, plot, poster, price, rating, stars, fav, isInCart } = props.movies;
 
 
-    render() {
-        const { title, plot, poster, price, rating, stars, fav, cart } = this.props.movies;
-        const { movies, addStars, decStars, handleCart, handleFav } = this.props;
-        return (
-            <div className="main">
-                <div className="movie-card">
-                    <div className="left">
-                        <img alt="Poster" src={poster} />
+    return (
+        //Movie Card
+        <div className="movie-card">
+
+            {/**Left section of Movie Card */}
+            <div className="left">
+                <img alt="poster" src={poster} />
+            </div>
+
+            {/**Right section Movie Card */}
+            <div className="right">
+
+                {/**Title, plot, price of the movie */}
+                <div className="title">{title}</div>
+                <div className="plot">{plot}</div>
+                <div className="price">Rs. {price}</div>
+
+                {/**Footer starts here with ratings, stars and buttons */}
+                <div className="footer">
+                    <div className="rating">{rating}</div>
+
+                    {/**Star image with increase and decrease buttons and star count */}
+                    <div className="star-dis">
+                        <img className="str-btn"
+                            alt="Decrease"
+                            src="https://cdn-icons-png.flaticon.com/128/2801/2801932.png"
+                            onClick={() => onDecStars(movies)}
+                        />
+                        <img className="stars"
+                            alt="stars"
+                            src="https://cdn-icons-png.flaticon.com/128/2107/2107957.png"
+                        />
+                        <img className="str-btn"
+                            alt="increase"
+                            src="https://cdn-icons-png.flaticon.com/128/2997/2997933.png"
+                            // No binding required as addStars() is an arrow function
+                            onClick={() => onIncStars(movies)}
+                        />
+                        <span className="starCount">{stars}</span>
                     </div>
-                    <div className="right">
-                        <div className="title">{title}</div>
-                        <div className="plot">{plot}</div>
-                        <div className="price">Rs. {price}</div>
 
-                        <div className="footer">
-                            <div className="rating">{rating}</div>
-                            <div className="star-dis">
-                                <img className="str-btn" alt="decrease" src="https://cdn-icons-png.flaticon.com/128/56/56889.png" onClick={() => decStars(movies)} />
+                    {/**conditional rendering on Favourite button */}
+                    <button className={fav ? "unfavourite-btn" : "favourite-btn"}
+                        onClick={() => onClickFav(movies)}>
 
-                                <img className="stars" alt="star" src="https://cdn-icons-png.flaticon.com/128/1828/1828884.png" />
+                        {fav ? "Un-favourite" : "Favourite"}
 
+                    </button>
 
-                                <img className="str-btn" alt="increase" src="https://cdn-icons-png.flaticon.com/128/3524/3524388.png" onClick={() => addStars(movies)} />
-                                <span>{stars}</span>
-                            </div>
-                            <button className={fav ? "favourite-btn" : "unfavourite-btn"} onClick={() => handleFav(movies)}> {fav ? "Un Favourite" : "Favourite"}</button>
-                            <button className={cart ? "cart-btn2" : "cart-btn"} onClick={() => handleCart(movies)}>{cart ? "Remove From Cart" : "Add to Cart"}</button>
-                        </div>
-                    </div>
+                    {/**Conditional Rendering on Add to Cart Button */}
+                    <button className={isInCart ? "unfavourite-btn" : "cart-btn"}
+                        onClick={() => onClickAddtocart(movies)}>
+
+                        {isInCart ? "Remove from Cart" : "Add to Cart"}
+
+                    </button>
+
                 </div>
-            </div >
-        );
-    }
+            </div>
+
+        </div>
+
+    )
 }
+
 
 export default MovieCard;
